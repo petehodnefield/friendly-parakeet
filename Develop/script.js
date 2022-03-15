372d2a7ca02addbcd838bf01af2015eedae3bca1
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-03-15 11:38:41 -0500

    window prompts for password requirements

--- Develop/script.js
+++ Develop/script.js
@@ -15,3 +15,15 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+document.getElementById('generate').onclick = function generatePassword() {
+
+  window.alert("These are the password requirements:");
+  window.alert("Password must be at least 14 characters long");
+  window.alert("Password must include at least one uppercase letter");
+  window.alert("Password must include at least one lowercase letter");
+  window.alert("Password must include at least one number");
+  window.alert("Password must include at least one special character");
+
+}
