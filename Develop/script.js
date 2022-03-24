01fb0a75a932b7f826b37ba4cf8e5529da188845
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-03-24 14:23:28 -0500

    added arrays and random generator loop

--- Develop/script.js
+++ Develop/script.js
@@ -1,56 +1,46 @@
-// Assignment code here
 
-// uppercase letters array
-var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+var passwordItems = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-// user defines the character count
+var upperCaseLetters = passwordItems.slice(0,26);
+var lowerCaseLetters = passwordItems.slice(26,52);
+var numbers = passwordItems.slice(52,63);
 
-// define generatePassword()
 var generatePassword = function() {
-  var passwordLength = window.prompt("Enter how many numbers long your password should be");
-  var includeUpperCase = prompt("Do you want your password to include uppercase letters? Enter 'Yes' to include and 'No' to not include them.");
-  
-    includeUpperCase.toLowerCase();
-    if(includeUpperCase === "yes") {
-      for (var i = 0; i < passwordLength; i++) {
-        console.log(upperCaseLetters[Math.floor(Math.random() * passwordLength)])
-      }
-    }
-    else if(includeUpperCase === "no") {
-      alert("You don't want upper case");
-    }
-    else {
-      alert("Please enter yes or no!");
-      return;
-    }
-    // randomly select fron the upperCaseLetters array
-    
+  // Ask user how long the password should be
+  var passwordLength = prompt("Enter how many characters you want you password to be");
 
-    for (var i = 0; i < lowerCaseLetters.length; i++) {
-      console.log(lowerCaseLetters[Math.floor(Math.random() * lowerCaseLetters.length)])
+  // Generate a random string of upperCaseLetters, lowerCaseLetters, and Numbers
+  for(i = 0; i < passwordLength; i++) {
+    console.log(upperCaseLetters[Math.floor(Math.random() * upperCaseLetters.length)]);
+    console.log(lowerCaseLetters[Math.floor(Math.random() * lowerCaseLetters.length)]);
+    console.log(numbers[Math.floor(Math.random() * numbers.length)]);
+  }
+ 
 
-    }
-    for (var i = 0; i < numbers.length; i++) {
-      console.log(numbers[Math.floor(Math.random() * numbers.length)])
+  // Prompt User with series of prompts for password criteria
+  // Prompt user if they want to include uppercase letters
+  // var includeUpperCase = window.prompt("Do you want uppercase letters? Enter YES to include, NO to exclude");
+  // // If the user wants uppercase letters, include them in the password generator
+  // includeUpperCase.toLowerCase();
+  // if (includeUpperCase === "yes") {
+  //   var randomUpperCase = upperCaseLetters[2];
+  //   console.log(randomUpperCase);
+  // }
+  // // Prompt user if they want to include lowercase letters
+  // window.prompt("Do you want lowercase letters? Enter YES to include, NO to exclude");
+  // // Prompt users if they want to include numbers
+  // window.prompt("Do you want numbers? Enter YES to include, NO to exclude");
 
-    }
 }
-
-
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
+
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
+// When the user clicks the generate password button, run the function
 generateBtn.addEventListener("click", writePassword);
