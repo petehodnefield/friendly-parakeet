484e88dcea6fc1a78d320029981be7aa45d0c28f
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-03-24 19:29:07 -0500

    fiddled with loop ideas

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,7 @@
 
 var generateBtn = document.querySelector("#generate");
 
+
 var passwordItems = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 var upperCaseLetters = passwordItems.slice(0,26);
@@ -11,33 +12,19 @@ var generatePassword = function() {
   // Ask user how long the password should be
   var passwordLength = prompt("Enter how many characters you want you password to be");
 
-  // Generate a random string of upperCaseLetters, lowerCaseLetters, and Numbers
-  for(i = 0; i < passwordLength; i++) {
-    console.log(upperCaseLetters[Math.floor(Math.random() * upperCaseLetters.length)]);
-    console.log(lowerCaseLetters[Math.floor(Math.random() * lowerCaseLetters.length)]);
-    console.log(numbers[Math.floor(Math.random() * numbers.length)]);
+  // Generate a random sequence of uppercase, lowercase, and numbers that matches the passwordLength variable
+  for(var i = 0; i < passwordLength; i++) {
+    // randomly select from upperCase lowerCase, and numbers,
+    passwordCharacters = upperCaseLetters[Math.floor(Math.random() * upperCaseLetters.length)];
+    console.dir(passwordCharacters);
   }
- 
-
-  // Prompt User with series of prompts for password criteria
-  // Prompt user if they want to include uppercase letters
-  // var includeUpperCase = window.prompt("Do you want uppercase letters? Enter YES to include, NO to exclude");
-  // // If the user wants uppercase letters, include them in the password generator
-  // includeUpperCase.toLowerCase();
-  // if (includeUpperCase === "yes") {
-  //   var randomUpperCase = upperCaseLetters[2];
-  //   console.log(randomUpperCase);
-  // }
-  // // Prompt user if they want to include lowercase letters
-  // window.prompt("Do you want lowercase letters? Enter YES to include, NO to exclude");
-  // // Prompt users if they want to include numbers
-  // window.prompt("Do you want numbers? Enter YES to include, NO to exclude");
-
+  return passwordCharacters;
 }
 function writePassword() {
   var password = generatePassword();
+  // console.dir(passwordText);
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
 }
